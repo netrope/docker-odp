743393a3baa04746a0581719ca470e8674fe7ebe
任健 <renjian@zuoyebang.com>
2018-12-29 20:02:17 +0800

    create

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-docker run -it -p 22:22 -v d:/website/homework/:/mnt/homework/ --privileged=true centos-odp /bin/bash
+docker run -it -p 22:22 -v //d/website/homework/:/mnt/homework/ --privileged=true centos-odp /bin/bash
 
 
